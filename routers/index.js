6e151723391ee677987073c6f75a68e6502e4d7d
sufu <fusu.robert@gmail.com>
2016-11-09 23:53:19 -0800

    Add index.html as /

---
+++ routers/index.js
@@ -0,0 +1,22 @@
+/**
+ * Created by sufu on 11/9/16.
+ */
+
+var express = require("express");
+var router = express.Router();
+
+router.get("/", function (req, res) {
+    var options = {root: __dirname + '/../public/views/'};
+    res.sendFile("index.html", options, function (err) {
+        if (err) {
+            console.log(err);
+            console.log("error sending index.html");
+        } else {
+            console.log("Sent: index.html");
+        }
+    });
+});
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,8 +6,10 @@ var express = require("express");
 var app = express();
 var restRouter = require("./routers/rest");
 var redirectRouter = require("./routers/redirect");
+var indexRouter = require("./routers/index");
 
 app.use("/api/v1", restRouter);
 app.use("/:shortUrl", redirectRouter);
+app.use("/", indexRouter);
 
 app.listen(3000);
